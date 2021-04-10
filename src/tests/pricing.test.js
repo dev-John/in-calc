7327f14267f7775f86f27bfec9a5fbc4a434e741
Jonatas <jonatasoliveira1234@gmail.com>
2021-04-10 17:40:48 -0300

    Finished

--- src/tests/pricing.test.js
+++ src/tests/pricing.test.js
@@ -20,14 +20,13 @@ afterAll((done) => {
 });
 
 describe("Integrates tests for the calc API", () => {
-  it.only("GET /get-pricing/9 - should return statusCode 400 and the error 'A metragem deve estar entre 10 e 10.000 metros'", async () => {
+  it("GET /get-pricing/9 - should return statusCode 400 and the error 'A metragem deve estar entre 10 e 10.000 metros'", async () => {
     const options = {
       method: GET,
       url: "/get-pricing/9",
     };
 
     const data = await server.inject(options);
-    console.log("🚀 ~ file: pricing.test.js ~ line 43 ~ it ~ data", data);
 
     expect(data.statusCode).toEqual(400);
     expect(data.result.message).toEqual(
@@ -35,14 +34,13 @@ describe("Integrates tests for the calc API", () => {
     );
   });
 
-  it.only("GET /get-pricing/10001 - should return statusCode 400 and the error 'A metragem deve estar entre 10 e 10.000 metros'", async () => {
+  it("GET /get-pricing/10001 - should return statusCode 400 and the error 'A metragem deve estar entre 10 e 10.000 metros'", async () => {
     const options = {
       method: GET,
       url: "/get-pricing/10001",
     };
 
     const data = await server.inject(options);
-    console.log("🚀 ~ file: pricing.test.js ~ line 58 ~ it ~ data", data);
 
     expect(data.statusCode).toEqual(400);
     expect(data.result.message).toEqual(
@@ -57,7 +55,6 @@ describe("Integrates tests for the calc API", () => {
     };
 
     const data = await server.inject(options);
-    console.log("🚀 ~ file: pricing.test.js ~ line 30 ~ it ~ data", data);
 
     expect(data.statusCode).toEqual(200);
     expect(data.result.totalValue).toEqual(784000);
